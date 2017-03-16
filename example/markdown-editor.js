acd31a03221fdf954905eaa55ded92775cf5e6f5
zoeying <zoeying@ibesteeth.com>
2017-03-16 18:58:37 +0800

    修改样式

--- example/markdown-editor.js
+++ example/markdown-editor.js
@@ -129,146 +129,22 @@ var bootstrapMarkdown = {
                 $htmlText.html( markdownit.render( e.getContent() ) );
                 markdownit.inlineStyles( $htmlText[0] ) ;
             } ,
+            // onShow : function( md ){
+            //     md.$textarea.siblings('.md-header').find('button').each( function( index , button ){
+            //         $(button).attr('data-toggle','tooltip')
+            //                  .attr('data-placement','top') ;
+            //     })
+            //     $('[data-toggle="tooltip"]').tooltip() ;
+            // },
             additionalButtons : [
-            [{
-                    name: "groupCustom",
-                    data: [{
-                    name: "cmdIntent",
-                    toggle: true, // this param only take effect if you load bootstrap.js
-                    title: "首行缩进文本",
-                    icon: { 'glyph' : "glyphicon glyphicon-indent-left" } ,
-                    callback: function(e){
-                        // Replace selection with some drinks
-                        var chunk,
-                            frontText , 
-                            contentText ,
-                            endText , 
-                            cursor,
-                            selected = e.getSelection() ;
-                            
-                        frontText = '\n::: intent <' ;
-                        contentText = '\n首行缩进文本' ;
-                        endText     = '\n:::\n' ;
-
-                        // Give random drink
-                        chunk = frontText + contentText + endText ;
-
-                        // transform selection and set the cursor into chunked text
-                        e.replaceSelection(chunk) ;
-                        cursor = selected.start + frontText.length + 1 ;
-                        // Set the cursor
-                        e.setSelection(cursor,cursor+contentText.length - 1) ;
-                    }
-                    },{
-                    name: "cmdMark",
-                    toggle: true, // this param only take effect if you load bootstrap.js
-                    title: "黄色强调",
-                    icon: { 'glyph' : "glyphicon glyphicon-flag" } ,
-                    callback: function(e){
-                        // Replace selection with some drinks
-                        var chunk, 
-                            cursor,
-                            selected = e.getSelection() ;
-                            
-                        // Give random drink
-                        selected.text ? chunk = '==' + selected.text + '==' : chunk = '==mark==' ;
-
-                        // transform selection and set the cursor into chunked text
-                        e.replaceSelection(chunk) ;
-                        cursor = selected.start + 2 ;
-                        // Set the cursor
-                        e.setSelection(cursor,cursor+chunk.length - 4 ) ;
-                    }
-                    },{
-                    name: "cmdNote",
-                    toggle: true, // this param only take effect if you load bootstrap.js
-                    title: "小灰字注释",
-                    icon: { 'glyph' : "glyphicon glyphicon-pencil" } ,
-                    callback: function(e){
-                        // Replace selection with some drinks
-                        var chunk,
-                            frontText , 
-                            contentText ,
-                            endText , 
-                            cursor,
-                            selected = e.getSelection() ;
-                            
-                        frontText = '\n::: note ' ;
-                        contentText = '写注释' ;
-                        endText     = '\n:::\n' ;
-
-                        // Give random drink
-                        chunk = frontText + contentText + endText ;
-
-                        // transform selection and set the cursor into chunked text
-                        e.replaceSelection(chunk) ;
-                        cursor = selected.start + frontText.length;
-                        // Set the cursor
-                        e.setSelection(cursor,cursor+contentText.length) ;
-                    }
-                    },{
-                    name: "cmdBlockquote",
-                    toggle: true, // this param only take effect if you load bootstrap.js
-                    title: "引用文字",
-                    icon: { 'glyph' : "glyphicon glyphicon-comment" } ,
-                    callback: function(e){
-                        // Replace selection with some drinks
-                        var chunk,
-                                frontText , 
-                                contentText ,
-                                endText , 
-                            cursor,
-                            selected = e.getSelection() ;
-                            
-                        frontText = '\n::: blockquote <' ;
-                        contentText = '\n引用' ;
-                        endText     = '\n:::' ;
-
-                        // Give random drink
-                        chunk = frontText + contentText + endText ;
-
-                        // transform selection and set the cursor into chunked text
-                        e.replaceSelection(chunk) ;
-                        cursor = selected.start + frontText.length + 1 ;
-                        // Set the cursor
-                        e.setSelection(cursor,cursor+contentText.length - 1) ;
-                    }
-                },{
-                    name: "cmdWireframe",
-                    toggle: true, // this param only take effect if you load bootstrap.js
-                    title: "线框注释",
-                    icon: { 'glyph' : "glyphicon glyphicon-bookmark" } ,
-                    callback: function(e){
-                        // Replace selection with some drinks
-                        var chunk,
-                            frontText , 
-                            contentText ,
-                            endText , 
-                            cursor,
-                            selected = e.getSelection() ;
-                            
-                        frontText = '\n::: wireframe ' ;
-                        contentText = '线框注释' ;
-                        endText     = '\n:::\n' ;
-
-                        // Give random drink
-                        chunk = frontText + contentText + endText ;
-
-                        // transform selection and set the cursor into chunked text
-                        e.replaceSelection(chunk) ;
-                        cursor = selected.start + frontText.length ;
-                        // Set the cursor
-                        e.setSelection(cursor,cursor+contentText.length ) ;
-                    }
-                    },{
-                    name: "cmdImg",
-                    toggle: true, // this param only take effect if you load bootstrap.js
-                    title: "图片链接",
-                    icon: { 'glyph' : "glyphicon glyphicon-picture" } ,
-                    callback: function(e){
-                        // 给一个弹框写图片链接
-                        var imgsrc = prompt("请输入图片地址","https://");
-                        if ( imgsrc!= null && imgsrc !="" ){                                
+                [{
+                        name: "groupCustom",
+                        data: [{
+                        name: "cmdIntent",
+                        toggle: true, // this param only take effect if you load bootstrap.js
+                        title: "首行缩进文本",
+                        icon: { 'glyph' : "glyphicon glyphicon-indent-left" } ,
+                        callback: function(e){
                             // Replace selection with some drinks
                             var chunk,
                                 frontText , 
@@ -277,9 +153,110 @@ var bootstrapMarkdown = {
                                 cursor,
                                 selected = e.getSelection() ;
                                 
-                            frontText = '::: img ![' ;
-                            contentText = '请输入图片描述' ;
-                            endText     = ']('+ imgsrc +')\n:::' ;
+                            frontText = '\n::: intent <' ;
+                            contentText = '\n首行缩进文本' ;
+                            endText     = '\n:::\n' ;
+
+                            // Give random drink
+                            chunk = frontText + contentText + endText ;
+
+                            // transform selection and set the cursor into chunked text
+                            e.replaceSelection(chunk) ;
+                            cursor = selected.start + frontText.length + 1 ;
+                            // Set the cursor
+                            e.setSelection(cursor,cursor+contentText.length - 1) ;
+                        }
+                        },{
+                        name: "cmdMark",
+                        toggle: true, // this param only take effect if you load bootstrap.js
+                        title: "黄色强调",
+                        icon: { 'glyph' : "glyphicon glyphicon-flag" } ,
+                        callback: function(e){
+                            // Replace selection with some drinks
+                            var chunk, 
+                                cursor,
+                                selected = e.getSelection() ;
+                                
+                            // Give random drink
+                            selected.text ? chunk = '==' + selected.text + '==' : chunk = '==mark==' ;
+
+                            // transform selection and set the cursor into chunked text
+                            e.replaceSelection(chunk) ;
+                            cursor = selected.start + 2 ;
+                            // Set the cursor
+                            e.setSelection(cursor,cursor+chunk.length - 4 ) ;
+                        }
+                        },{
+                        name: "cmdNote",
+                        toggle: true, // this param only take effect if you load bootstrap.js
+                        title: "小灰字注释",
+                        icon: { 'glyph' : "glyphicon glyphicon-pencil" } ,
+                        callback: function(e){
+                            // Replace selection with some drinks
+                            var chunk,
+                                frontText , 
+                                contentText ,
+                                endText , 
+                                cursor,
+                                selected = e.getSelection() ;
+                                
+                            frontText = '\n::: note ' ;
+                            contentText = '写注释' ;
+                            endText     = '\n:::\n' ;
+
+                            // Give random drink
+                            chunk = frontText + contentText + endText ;
+
+                            // transform selection and set the cursor into chunked text
+                            e.replaceSelection(chunk) ;
+                            cursor = selected.start + frontText.length;
+                            // Set the cursor
+                            e.setSelection(cursor,cursor+contentText.length) ;
+                        }
+                        },{
+                        name: "cmdBlockquote",
+                        toggle: true, // this param only take effect if you load bootstrap.js
+                        title: "引用文字",
+                        icon: { 'glyph' : "glyphicon glyphicon-comment" } ,
+                        callback: function(e){
+                            // Replace selection with some drinks
+                            var chunk,
+                                    frontText , 
+                                    contentText ,
+                                    endText , 
+                                cursor,
+                                selected = e.getSelection() ;
+                                
+                            frontText = '\n::: blockquote <' ;
+                            contentText = '\n引用' ;
+                            endText     = '\n:::' ;
+
+                            // Give random drink
+                            chunk = frontText + contentText + endText ;
+
+                            // transform selection and set the cursor into chunked text
+                            e.replaceSelection(chunk) ;
+                            cursor = selected.start + frontText.length + 1 ;
+                            // Set the cursor
+                            e.setSelection(cursor,cursor+contentText.length - 1) ;
+                        }
+                    },{
+                        name: "cmdWireframe",
+                        toggle: true, // this param only take effect if you load bootstrap.js
+                        title: "线框注释",
+                        icon: { 'glyph' : "glyphicon glyphicon-bookmark" } ,
+                        callback: function(e){
+                            // Replace selection with some drinks
+                            var chunk,
+                                frontText , 
+                                contentText ,
+                                endText , 
+                                cursor,
+                                selected = e.getSelection() ;
+                                
+                            frontText = '\n::: wireframe ' ;
+                            contentText = '线框注释' ;
+                            endText     = '\n:::\n' ;
 
                             // Give random drink
                             chunk = frontText + contentText + endText ;
@@ -288,11 +265,53 @@ var bootstrapMarkdown = {
                             e.replaceSelection(chunk) ;
                             cursor = selected.start + frontText.length ;
                             // Set the cursor
-                            e.setSelection(cursor,cursor+contentText.length) ;
+                            e.setSelection(cursor,cursor+contentText.length ) ;
                         }
-                    }
+                        },{
+                        name: "cmdImg",
+                        toggle: true, // this param only take effect if you load bootstrap.js
+                        title: "图片链接",
+                        icon: { 'glyph' : "glyphicon glyphicon-picture" } ,
+                        callback: function(e){
+                            // 给一个弹框写图片链接
+                            var imgsrc = prompt("请输入图片地址","https://");
+                            if ( imgsrc!= null && imgsrc !="" ){                                
+                                // Replace selection with some drinks
+                                var chunk,
+                                    frontText , 
+                                    contentText ,
+                                    endText , 
+                                    cursor,
+                                    selected = e.getSelection() ;
+                                    
+                                frontText = '::: img ![' ;
+                                contentText = '请输入图片描述' ;
+                                endText     = ']('+ imgsrc +')\n:::' ;
+
+                                // Give random drink
+                                chunk = frontText + contentText + endText ;
+
+                                // transform selection and set the cursor into chunked text
+                                e.replaceSelection(chunk) ;
+                                cursor = selected.start + frontText.length ;
+                                // Set the cursor
+                                e.setSelection(cursor,cursor+contentText.length) ;
+                            }
+                        }
+                        }]
+                },{
+                    name: "groupInfo",
+                    data: [{
+                        name: "cmdHelp",
+                        toggle: true, // this param only take effect if you load bootstrap.js
+                        title: "help",
+                        icon: { 'glyph' : "glyphicon glyphicon-question-sign" } ,
+                        callback: function(e){
+                            // Replace selection with some drinks
+                            window.open('http://www.appinn.com/markdown/','_blank');
+                        }
                     }]
-            }]
+                }]
             ]
         } ;
         return $.extend( config , opt , false );
